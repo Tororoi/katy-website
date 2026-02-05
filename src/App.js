fcc24a4ab84f7258f5af0367fa08cdb45924772a
Tororoi <cantwell.tom@gmail.com>
2026-02-04 20:26:13 -0800

    update packages

--- src/App.js
+++ src/App.js
@@ -1,69 +1,59 @@
-import React from "react"
-import "./App.css"
-import NavBar from "./Components/NavBar"
-import ProjectContainer from "./Components/ProjectContainer"
-import ProjectDisplay from "./Components/ProjectDisplay"
-import AboutPage from "./Components/AboutPage"
-import ExhibitionsContainer from "./Components/ExhibitionsContainer"
-import MobileNav from "./Components/MobileNav"
-import Contact from "./Components/Contact"
-import ShopPage from "./Components/ShopPage"
+import React from 'react'
+import './App.css'
+import NavBar from './Components/NavBar'
+import ProjectContainer from './Components/ProjectContainer'
+import ProjectDisplay from './Components/ProjectDisplay'
+import AboutPage from './Components/AboutPage'
+import ExhibitionsContainer from './Components/ExhibitionsContainer'
+import MobileNav from './Components/MobileNav'
+import Contact from './Components/Contact'
+import ShopPage from './Components/ShopPage'
 // import Media from './Components/Media'
 
-import { connect } from "react-redux"
+import { connect } from 'react-redux'
+import { Routes, Route } from 'react-router-dom'
 
-import { BrowserRouter as Router, Route, withRouter } from "react-router-dom"
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <div className="site-content">
-            <header>
-              <NavBar toggleNav={this.props.toggleNav} />
-              <MobileNav toggleNav={this.props.toggleNav} />
-            </header>
-            <main>
-              <Route
-                path="/exhibitions"
-                exact
-                render={() => <ExhibitionsContainer />}
-              />
-              <Route path="/bio" exact render={() => <AboutPage />} />
-              <Route
-                path="/project"
-                exact
-                render={() => (
-                  <ProjectDisplay toggleNav={this.props.toggleNav} />
-                )}
-              />
-              <Route
-                path="/gallery"
-                exact
-                render={() => <ProjectContainer />}
-              />
-              <Route path="/shop" exact render={() => <ShopPage />} />
-              <Route path="/contact" exact render={() => <Contact />} />
-              <Route path="/" exact render={() => <ProjectContainer />} />
-              {/* <Media/> */}
-            </main>
-          </div>
-        </Router>
-      </div>
-    )
-  }
-}
-
-let toggleNav = (boolean) => {
+const toggleNav = (boolean) => {
   return {
-    type: "TOGGLE_NAV",
+    type: 'TOGGLE_NAV',
     payload: boolean,
   }
 }
 
-let sendThisInformation = {
+const sendThisInformation = {
   toggleNav,
 }
 
-export default withRouter(connect(null, sendThisInformation)(App))
+const AppContent = ({ toggleNav }) => {
+  return (
+    <div className="App">
+      <div className="site-content">
+        <header>
+          <NavBar toggleNav={toggleNav} />
+          <MobileNav toggleNav={toggleNav} />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/exhibitions" element={<ExhibitionsContainer />} />
+            <Route path="/bio" element={<AboutPage />} />
+            <Route
+              path="/project"
+              element={<ProjectDisplay toggleNav={toggleNav} />}
+            />
+            <Route path="/gallery" element={<ProjectContainer />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/" element={<ProjectContainer />} />
+            {/* <Media/> */}
+          </Routes>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+const App = ({ toggleNav }) => {
+  return <AppContent toggleNav={toggleNav} />
+}
+
+export default connect(null, sendThisInformation)(App)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 import data from './data'
 
 // redux dependencies
@@ -9,7 +9,7 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 // routing dependencies
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 //Initial State
 let initialState = {
@@ -19,71 +19,75 @@ let initialState = {
   project: data.artwork[0],
   exhibitions: [...data.exhibitions],
   residencies: [...data.residencies],
-  contact: {name: '', email: '', subject: '', message: ''},
-  fullImage: false
+  contact: { name: '', email: '', subject: '', message: '' },
+  fullImage: false,
 }
 
 // if an action gets dispatched, that action will be ran through all of the reducers
 let theReducer = (state = initialState, action) => {
-
   switch (action.type) {
-
-    case "TOGGLE_NAV":
-
+    case 'TOGGLE_NAV':
       return {
         ...state,
-        mobileNav: !state.mobileNav
+        mobileNav: !state.mobileNav,
       }
 
-    case "CHOOSE_PROJECT":
-
+    case 'CHOOSE_PROJECT':
       return {
         ...state,
-        project: action.payload
+        project: action.payload,
       }
-    
-    case "PREV_NEXT":
+
+    case 'PREV_NEXT':
       let projIndex = state.project.id
-      const dir = action.payload;
-      dir === "◀" ? projIndex -= 1 : projIndex += 1
-      if (projIndex < 0) {projIndex = 9;} else if (state.projects[projIndex] === undefined) {projIndex = 0;}
+      const dir = action.payload
+      dir === '◀' ? (projIndex -= 1) : (projIndex += 1)
+      if (projIndex < 0) {
+        projIndex = 9
+      } else if (state.projects[projIndex] === undefined) {
+        projIndex = 0
+      }
 
       return {
         ...state,
-        project: state.projects[projIndex]
+        project: state.projects[projIndex],
       }
-    case "UPDATE_CONTACT":
-      let name = action.payload.name;
-      let value = action.payload.value;
+    case 'UPDATE_CONTACT':
+      let name = action.payload.name
+      let value = action.payload.value
       return {
         ...state,
         contact: {
           ...state.contact,
-          [name]: value
-        }
+          [name]: value,
+        },
       }
-    case "DISPLAY_FULL_IMAGE":
-      let newState = !state.fullImage;
-      let color = "black";
-      newState ? color = "black" : color = "white";
-      document.body.style.backgroundColor = color;
+    case 'DISPLAY_FULL_IMAGE':
+      let newState = !state.fullImage
+      let color = 'black'
+      newState ? (color = 'black') : (color = 'white')
+      document.body.style.backgroundColor = color
       return {
         ...state,
-        fullImage: newState
+        fullImage: newState,
       }
     default:
       return state
   }
-
 }
 
-let storeObject = createStore(theReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+let storeObject = createStore(
+  theReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={storeObject}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={storeObject}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
